7db7b7e68d767a135dab40e005d6fff41c802017
neerajsinghrawat710 <neerajsinghrawat710@gmail.com>
2023-04-23 16:09:57 +0530

    code updated

--- src/comman/NoDataFound.jsx
+++ src/comman/NoDataFound.jsx
@@ -1,13 +1,14 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import PropTypes from 'prop-types';
+import NoDataFoundImg from "../assets/img/noData.jpg"
 
 
 export default function NoDataFound({ height, }) {
 
     return (
         <Box sx={{ height: height ? height : "100%", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <img src="/assets/img/noData.jpg" height={"100%"} />
+            <img src={NoDataFoundImg} height={"100%"} alt='no_data' />
         </Box>
     )
 }
